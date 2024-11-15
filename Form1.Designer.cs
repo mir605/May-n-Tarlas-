e41beccf8e9a6edc29ae73c792cd497c535b4edd
mir605 <225541001@firat.edu.tr>
2024-11-15 21:55:02 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Mayın_Tarlası
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.flowLayoutPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("flowLayoutPanel1.BackgroundImage")));
+            this.flowLayoutPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(31, 31);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(512, 359);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(642, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 84);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CREATOR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Location = new System.Drawing.Point(505, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Puan";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mayın_Tarlası
+{
+    public partial class Form1 : Form
+    {
+
+        Random random = new Random();
+        int puan = 0;
+
+        //Tahtanın ebatı
+        int[,] ButtonValue = new int[7, 9];
+        public Form1()  
+
+        
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ButtonCreator();
+
+        }
+
+        public void ButtonCreator()
+        {
+            flowLayoutPanel1.Controls.Clear();
+
+            for (int i = 0; i < 7; i++)
+            {
+                for (int j=0; j <9; j++)
+                {
+                    Button button = new Button();
+                    button.Text = string.Empty;
+
+                    int randomDeger = random.Next(0, 2);
+                    button.Tag = randomDeger;
+                    button.BackColor = Color.BlueViolet;
+                    button.Size = new System.Drawing.Size(34, 34);
+
+
+                    flowLayoutPanel1.Controls.Add(button);
+                    button.Click += ButtonClicker;
+
+                    ButtonValue[i, j] = randomDeger;
+                }
+            }
+
+        }
+      
+
+       
+
+
+        public void ButtonClicker(object sender, EventArgs e)
+        {
+            
+            Button clickedButton = sender as Button;
+
+            if(clickedButton!= null)
+            {
+                int value = (int)clickedButton.Tag;
+                //sıfıra tıklanınca oyun biter buton kırmızıya döner.
+                if(value==0)
+                {
+                    clickedButton.BackColor = Color.Red;
+                    
+                    MessageBox.Show($"Oyunu kaybettiniz çünkü mayına bastınız\n Puanınız: {puan}");
+                    puan = 0;
+                    //oyun tahtası sıfırlanır
+                    flowLayoutPanel1.Controls.Clear();
+                    PuanEkleyici();
+
+                }
+
+                //butonun değeri 1 ise rengi yeşile çevir puanı 1 arttır
+                if(value==1) 
+                {
+                    clickedButton.BackColor = Color.Green;
+                    
+                    puan++;
+                    PuanEkleyici();//puanı güncelle
+                }
+               
+                
+            }
+        }
+
+       
+
+
+      
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void PuanEkleyici()
+        {
+            label1.Text = "Puan: " + puan.ToString();
+        }
+
+    }
+}
